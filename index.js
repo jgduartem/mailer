68a2f5f5edf1bd326015bd6008b4f86115383fb2
Jose Duarte <joseduarte019@gmail.com>
2021-02-08 22:23:38 -0400

    pachumailer created

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(cors({origin: true}))
+
+app.post('/api', (req, res) => {
+    const {body} = req;
+    const isValidMessage = body.message && body.to && body.subject
+    if(!isValidMessage){
+        return res.status(400).send({message: 'invalid request'})
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASS
+        }
+    })
+
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: body.to,
+        subject: body.subject,
+        text: body.message
+    }
+
+    transporter.sendMail(mailOptions, (err, data) => {
+        if(err){
+            return res.status(500).send({message: error} + err.message)
+        }
+
+        return res.send({message: 'email.send'})
+    })
+})
+
+app.listen(port, () => {
+    console.log('app runing on port', process.env.PORT)
+});
